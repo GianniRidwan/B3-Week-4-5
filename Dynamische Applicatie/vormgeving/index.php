c2f779e05ffbcae69ae8143268e30a90668288f3
Gianni Ridwan <99036552@mydavinci.nl>
2022-03-17 10:13:51 +0100

    code aangepast voor php

--- Dynamische Applicatie/vormgeving/index.php
+++ Dynamische Applicatie/vormgeving/index.php
@@ -13,30 +13,30 @@
     ?>
 
 <?php
-    $stmt = $conn->prepare ("SELECT name, health, attack, defense FROM characters ORDER BY name ASC");
+    $stmt = $conn->prepare ("SELECT name, avatar, health, attack, defense FROM characters ORDER BY name ASC");
     $stmt->execute();
     $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
     foreach ($result as $row){
         // echo $row['name'] . $row['health'] . $row['attack'] . $row['defense'] . '<br/>';
    
-    echo '<div id="container">';
-        echo '<a class="item" href="character.php">';
-            echo '<div class="left">';
-                echo '<img class="avatar" src="resources/images/bowser.jpg">';
-            echo '</div>';
-        echo '<div class="right">';
-            echo '<h2>' . $row['name'] . '</h2>';
-                echo '<div class="stats">';
-                    echo '<ul class="fa-ul">';
-                        echo '<li><span class="fa-li"><i class="fas fa-heart"></i></span>' . $row["health"] . '</li>';
-                        echo '<li><span class="fa-li"><i class="fas fa-fist-raised"></i></span>' . $row['attack'] . '</li>';
-                        echo '<li><span class="fa-li"><i class="fas fa-shield-alt"></i></span>' . $row['defense'] . '</li>';
-                    echo '</ul>';
-                echo '</div>';
-            echo '</div>';
-        echo '<div class="detailButton"><i class="fas fa-search"></i> bekijk</div>';
-        echo '</a>';
-    echo '</div>';
+    echo '<div id="container">' . 
+            '<a class="item" href="character.php">' . 
+                '<div class="left">' . 
+                    '<img class="avatar" src="resources/images/' . $row['avatar'] . '">' . 
+                '</div>' . 
+                '<div class="right">' . 
+                    '<h2>' . $row['name'] . '</h2>' . 
+                    '<div class="stats">' . 
+                        '<ul class="fa-ul">' . 
+                            '<li><span class="fa-li"><i class="fas fa-heart"></i></span>' . $row["health"] . '</li>' . 
+                            '<li><span class="fa-li"><i class="fas fa-fist-raised"></i></span>' . $row['attack'] . '</li>' . 
+                            '<li><span class="fa-li"><i class="fas fa-shield-alt"></i></span>' . $row['defense'] . '</li>' . 
+                        '</ul>' . 
+                    '</div>' . 
+                '</div>' . 
+            '<div class="detailButton"><i class="fas fa-search"></i> bekijk</div>' . 
+            '</a>' . 
+    '</div>';
     }
 ?>
 
